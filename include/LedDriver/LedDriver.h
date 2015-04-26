46a90510e7b0363f965c7bb91340a13620c61d11
Gary Tempus, Jr <gtempus@codegaloot.com>
2015-04-26 03:14:37 +0000

    Test runner is running the only test and it's failing.

---
+++ include/LedDriver/LedDriver.h
@@ -0,0 +1,7 @@
+#ifndef D_LedDriver_H
+#define D_LedDriver_H
+
+void LedDriver_Create(void);
+void LedDriver_Destroy(void);
+
+#endif  /* D_LedDriver_H */
---
+++ tests/LedDriver/LedDriverTest.cpp
@@ -0,0 +1,21 @@
+extern "C"
+{
+#include "LedDriver.h"
+}
+
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(LedDriver) {
+  void setup() {
+    LedDriver_Create();
+  }
+
+  void teardown() {
+    LedDriver_Destroy();
+  }
+};
+
+TEST(LedDriver, Create) {
+  FAIL("Start here");
+}
+
