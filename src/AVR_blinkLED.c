0ea5066a5ccc35c8acb04dfff2f10571ca14f6d5
Gary Tempus, Jr <gtempus@codegaloot.com>
2015-05-08 03:00:52 +0000

    Just had to try it; butterfly.

--- src/AVR_blinkLED.c
+++ src/AVR_blinkLED.c
@@ -11,21 +11,33 @@ void RuntimeError(const char* m, int p, const char* f, int l) {
   return; //need to log this!
 }
 
+void POVDisplay(uint8_t oneByte) {
+  PORTB = oneByte;
+  _delay_ms(5);
+}
+
 int main(void) {
   // -------- Inits --------- //
-  DDRB |= 0b0000001;            /* Data Direction Register B:
+  DDRB |= 0b11111111;            /* Data Direction Register B:
                                    writing a one to the bit
                                    enables output. */
-  LedDriver_Create(&PORTB);
-
   while (1) {
+    POVDisplay(0b00001100);
+    POVDisplay(0b01010010);
+    POVDisplay(0b10100010);
+    POVDisplay(0b10100100);
+    POVDisplay(0b11001001);
+    POVDisplay(0b01010010);
+    POVDisplay(0b11111100);
+    POVDisplay(0b01010010);
+    POVDisplay(0b11001001);
+    POVDisplay(0b10100100);
+    POVDisplay(0b10100010);
+    POVDisplay(0b01010010);
+    POVDisplay(0b00001100);
 
-    LedDriver_TurnOn(1);
-    _delay_ms(8);
-
-    LedDriver_TurnOff(1);
-    _delay_ms(1000);
-
+    PORTB = 0;
+    _delay_ms(40);
   }
   
   return (0);
